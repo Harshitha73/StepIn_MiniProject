297bdbe298c67db63fed0b2ae936b9047c18045e
HARSHITHA <harshithachava@gmail.com>
2021-04-14 20:25:37 +0530

    Upadated main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-    printf("\n****Sortingr****\n"); // gcc -I inc -Iunity  project_main.c src\BubbleSort.c src\Swap.c src\Display.c src\InsertionSort
+    printf("\n****Sorting****\n"); // gcc -I inc -Iunity  project_main.c src\BubbleSort.c src\Swap.c src\Display.c src\InsertionSort
     while(1)
     {
         Sorting_menu();
