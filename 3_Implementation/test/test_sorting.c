aad4e154b6bf6249fb7563364a8f574937653e38
HARSHITHA <harshithachava@gmail.com>
2021-04-14 16:44:49 +0530

    Update test_sorting

--- 3_Implementation/test/test_sorting.c
+++ 3_Implementation/test/test_sorting.c
@@ -1,11 +1,20 @@
 #include "unity.h"
 
 
-/* Modify these two lines according to the project */
+/**
+ * @file test_sorting.c
+ * @author Harshitha
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "Sorting.h"
 #define PROJECT_NAME    "Sorting"
 
-/* Prototypes for all the test functions */
+// Prototypes for all the test functions 
 
 void test_BubbleSort(void);
 void test_InsertionSort(void);
@@ -14,29 +23,28 @@ void test_QuickSort(void);
 void test_MergeSort(void);
 
 
-/* Required by the unity test framework */
+// Required by the unity test framework */
 void setUp(){}
-/* Required by the unity test framework */
 void tearDown(){}
 
-/* Start of the application test */
+// Start of the application test 
 int main()
 {
-/* Initiate the Unity Test Framework */
+// Initiate the Unity Test Framework 
   UNITY_BEGIN();
 
-/* Run Test functions */
+// Run Test functions 
   RUN_TEST(test_BubbleSort);
   RUN_TEST(test_InsertionSort);
   RUN_TEST(test_SelectionSort);
   RUN_TEST(test_QuickSort);
   RUN_TEST(test_MergeSort);
   
-  /* Close the Unity Test Framework */
+  // Close the Unity Test Framework 
   return UNITY_END();
 }
 
-/* Write all the test functions */ 
+// Write all the test functions  
 void test_BubbleSort(void) {
  int expected[]={10, 12, 13};
    int list[]={10, 13, 12};
@@ -61,11 +69,6 @@ void test_SelectionSort(void) {
    int* actual=SelectionSort(list,3);
    TEST_ASSERT_EQUAL_INT_ARRAY(expected,actual,3);
   
-  /* Dummy fail*/
-    int expected1[]={3,4,10, 12};
-    int list1[]={10,12,4,3};
-    int* actual1=SelectionSort(list1,4);
-    TEST_ASSERT_EQUAL_INT_ARRAY(expected1,actual1,4);
 }
 
 void test_QuickSort(void) {
