bf27d7df8b5a152f3e17153e136b977f4c3eaac6
HARSHITHA <harshithachava@gmail.com>
2021-04-14 16:46:21 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,5 +1,11 @@
 #include "Sorting.h"
-
+/**
+ * @brief main function of the program
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char *argv[])
 {
     printf("\n****Sortingr****\n"); // gcc -I inc -Iunity  project_main.c src\BubbleSort.c src\Swap.c src\Display.c src\InsertionSort
